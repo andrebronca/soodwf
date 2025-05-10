c023955af9266aff635a6f407c8331e92c3f4570
andre.bronca <bronca.andre@gmail.com>
2025-05-10 11:01:09 -0300

    Conclusão de js POO

--- Formacao/01_javascript_typescript/js_002_OO/Cliente.js
+++ Formacao/01_javascript_typescript/js_002_OO/Cliente.js
@@ -1,4 +1,40 @@
+import { ContaCorrente } from "./ContaCorrente.js";
+
+// A maior parte da classe eu fiz adapatações
 export class Cliente {
-    nome;
-    cpf;
+    _nome;
+    _cpf;
+    _ContaCorrente;
+
+    // constructor(nome, cpf) { //só pode ter um construtor
+    //     this._nome = nome;
+    //     this._cpf = cpf;
+    // }
+
+    constructor(nome, cpf, conta_corrente) {
+        this._nome = nome;
+        this._cpf = cpf;
+        this.contaCorrente = conta_corrente;
+    }
+
+    set contaCorrente(conta) {
+        if (conta instanceof ContaCorrente)
+            this._ContaCorrente = conta;
+    }
+
+    get contaCorrente() {
+        return this._ContaCorrente;
+    }
+
+    get nome() {
+        return this._nome;
+    }
+
+    get cpf() {
+        return this._cpf;
+    }
+
+    _valida(conta) {
+        return conta != undefined && conta != null;
+    }
 }
--- Formacao/01_javascript_typescript/js_002_OO/ContaCorrente.js
+++ Formacao/01_javascript_typescript/js_002_OO/ContaCorrente.js
@@ -1,6 +1,28 @@
+
+// A maior parte da classe eu fiz adapatações
 export class ContaCorrente {
-    agencia;
-    _saldo = 0;
+    static _totalContas = 0;    //impedir alterações do valor
+    _agencia;
+    _saldo;
+    _verComprovante = false;
+
+    constructor(agencia, saldo) {
+        this._agencia = agencia;
+        this._saldo = saldo;
+        ContaCorrente._totalContas++;
+    }
+
+    static get totalContas() {
+        return ContaCorrente._totalContas;
+    }
+
+    set _saldo(vlr) {
+        this._saldo = vlr < 0 ? 0 : vlr;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
 
     sacar(valor) {
         if (this._saldo >= valor) {
@@ -8,7 +30,7 @@ export class ContaCorrente {
             this._comprovante();
             return valor;
         }
-        //não há saldo suficiente para sacar
+        // não há saldo suficiente para sacar
     }
 
     depositar(valor) {
@@ -19,7 +41,24 @@ export class ContaCorrente {
         this._comprovante();
     }
 
+    _temSaldo(valor) {
+        return this._saldo >= valor;
+    }
+
+    transferir(valor, conta_destino) {
+        if (this._temSaldo(valor) && this._validaDestino(conta_destino)) {
+            this.sacar(valor);
+            conta_destino.depositar(valor);
+        }
+    }
+
+    _validaDestino(conta) {
+        return conta != undefined && conta != null;
+    }
+
     _comprovante() {
-        console.log(`Saldo atual: ${this._saldo}`);
+        if (this._verComprovante) {
+            console.log(`Agencia: ${this.agencia}: Saldo atual: ${this._saldo}`);
+        }
     }
 }
