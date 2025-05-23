ad79b6237407c41b425bb5f59bbf5c8633d97214
andre.bronca <bronca.andre@gmail.com>
2025-05-22 23:14:19 -0300

    start

--- Formacao/01_javascript_typescript/ts_002/chapter02/index.ts
+++ Formacao/01_javascript_typescript/ts_002/chapter02/index.ts
@@ -3,23 +3,23 @@ let myNum = 10;
 let anotherNum: number = 20;
 
 myNum = 12;
-myNum = '12';
+//myNum = '12';
 
 anotherNum = 30;
-anotherNum = false;
+//anotherNum = false;
 
 //String
 let myStr: string = 'Hello';
 let anotherStr = 'World';
-myStr = true;
-anotherStr = 45;
+//myStr = true;
+//anotherStr = 45;
 
 //Boolean
 let myBool: boolean = true;
 let anotherBool = false;
 
-myBool = 'true';
-anotherBool = 76;
+//myBool = 'true';
+//anotherBool = 76;
 
 //Inference (o tipo não foi definido na criação)
 //não é a forma recomendada
@@ -31,8 +31,8 @@ salary = true;
 //Inference
 let remuneracao: number;
 remuneracao = 12000;
-remuneracao = '12000';
-remuneracao = true;
+//remuneracao = '12000';
+//remuneracao = true;
 
 
 //Objects (página xiii (19 / 172))
