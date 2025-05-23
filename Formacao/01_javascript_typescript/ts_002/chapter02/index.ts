3b6c05a913f6bc0779bc7b8b89f5dabc578c17fb
andre.bronca <bronca.andre@gmail.com>
2025-05-23 07:25:42 -0300

    sintaxe

--- Formacao/01_javascript_typescript/ts_002/chapter02/index.ts
+++ Formacao/01_javascript_typescript/ts_002/chapter02/index.ts
@@ -36,3 +36,78 @@ remuneracao = 12000;
 
 
 //Objects (página xiii (19 / 172))
+const developer = {
+    firstName: 'Nabendu',
+    lastName: 'Biswas',
+    age: 40,
+    isTrainer: true
+}
+
+const newDeveloper: { name: string; age: number; isDev: boolean } = {
+    name: 'Mousam',
+    age: 39,
+    isDev: true
+}
+
+newDeveloper.name = 'Mousam Mishra';
+//newDeveloper.age = 'Forty';
+//newDeveloper.firstName = 'Mousam';
+
+// Arrays
+const languages = ['React', 'Angular', 'Vue'];
+
+languages.push('TypeScript');
+// languages.push(56);
+// languages.push(true);
+const numbers: number[] = [51, 21, 35];
+numbers.push(56);
+// numbers.push('57');
+// numbers.push(true);
+
+// Complex Arrays
+const arrOfObj: { name: string; age: number }[] = [
+    { name: 'Nabendu', age: 40 },
+    { name: 'Mousam', age: 39 }
+];
+
+const arrOfArrays: number[][] = [
+    [11, 32, 43],
+    [34, 75, 64]
+];
+
+arrOfArrays.push([21, 32, 13]);
+
+// Functions
+const addNums = (num1, num2) => { return num1 + num2; }
+addNums(10, 20);
+addNums(10, '20');
+
+const multiNums = (num1: number, num2: number) => {
+    return num1 * num2;
+}
+
+multiNums(10, 20);
+// multiNums(10, '20');
+
+const modNums = (num1: number, num2: number): number => {
+    return num1 % num2;
+    // return num1 > num2;
+}
+
+modNums(10, 20);
+// modNums(10, '20');
+
+const printSum = (num1: number, num2: number): void => {
+    console.log(num1 + num2);
+}
+
+printSum(10, 20);
+// printSum(10, '20');
+
+// Union Types
+let numOrStr: number | string;
+numOrStr = 10;
+numOrStr = 'Ten';
+
+let arr: (number | string)[] = [10, 'Ten', true];
+
