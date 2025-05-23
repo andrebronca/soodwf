470b486a80fd84a55d1afdaa07cbc73a4536b53e
andre.bronca <bronca.andre@gmail.com>
2025-05-22 23:09:37 -0300

    start

---
+++ Formacao/01_javascript_typescript/ts_002/chapter02/index.js
@@ -0,0 +1,28 @@
+//Numbers
+var myNum = 10;
+var anotherNum = 20;
+myNum = 12;
+myNum = '12';
+anotherNum = 30;
+anotherNum = false;
+//String
+var myStr = 'Hello';
+var anotherStr = 'World';
+myStr = true;
+anotherStr = 45;
+//Boolean
+var myBool = true;
+var anotherBool = false;
+myBool = 'true';
+anotherBool = 76;
+//Inference (o tipo não foi definido na criação)
+//não é a forma recomendada
+var salary;
+salary = 12000;
+salary = '12000';
+salary = true;
+//Inference
+var remuneracao;
+remuneracao = 12000;
+remuneracao = '12000';
+remuneracao = true;
---
+++ Formacao/01_javascript_typescript/ts_002/chapter02/index.ts
@@ -0,0 +1,38 @@
+//Numbers
+let myNum = 10;
+let anotherNum: number = 20;
+
+myNum = 12;
+myNum = '12';
+
+anotherNum = 30;
+anotherNum = false;
+
+//String
+let myStr: string = 'Hello';
+let anotherStr = 'World';
+myStr = true;
+anotherStr = 45;
+
+//Boolean
+let myBool: boolean = true;
+let anotherBool = false;
+
+myBool = 'true';
+anotherBool = 76;
+
+//Inference (o tipo não foi definido na criação)
+//não é a forma recomendada
+let salary;
+salary = 12000;
+salary = '12000';
+salary = true;
+
+//Inference
+let remuneracao: number;
+remuneracao = 12000;
+remuneracao = '12000';
+remuneracao = true;
+
+
+//Objects (página xiii (19 / 172))
