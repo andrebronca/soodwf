157330ad9356ba9bbc8d89c5982608a85b2b2794
andre.bronca <bronca.andre@gmail.com>
2025-05-29 20:59:48 -0300

    Configurando e testando o MongoDB

--- Formacao/02_express_js/express_001/chapter_03/index.js
+++ Formacao/02_express_js/express_001/chapter_03/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const path = require('path')
 const ejs = require('ejs')
+const mongoose = require('mongoose')
 
 const app = new express()
 app.set('view engine', 'ejs')
-
 app.use(express.static('public'))
+mongoose.connect('mongodb://localhost/my_database')
+
 
 app.listen(3000, () => {
     console.log('App listening on port 3000')
